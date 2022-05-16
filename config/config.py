77788618502d6584b23686a23ce6478d5056ec69
luzhangao <luzhangao@gmail.com>
2022-05-15 23:11:27 -0400

    Update config

--- config/config.py
+++ config/config.py
@@ -34,9 +34,44 @@ vkey = {
     "8 key": 0x38,
     "9 key": 0x39,
 
+    "A key": 0x41,
+    "B key": 0x42,
+    "C key": 0x43,
+    "D key": 0x44,
+    "E key": 0x45,
+    "F key": 0x46,
+    "G key": 0x47,
+    "H key": 0x48,
+    "I key": 0x49,
+    "J key": 0x4A,
+    "K key": 0x4B,
+    "L key": 0x4C,
+    "M key": 0x4D,
+    "N key": 0x4E,
+    "O key": 0x4F,
+    "P key": 0x50,
+    "Q key": 0x51,
+    "R key": 0x52,
+    "S key": 0x53,
+    "T key": 0x54,
+    "U key": 0x55,
+    "V key": 0x56,
+    "W key": 0x57,
+    "X key": 0x58,
+    "Y key": 0x59,
     "Z key": 0x5A,
     "ALT key": 0x12,
     "space key": 0x20,
+
+    "left windows key": 0x5B,
+    "right windows key": 0x5C,
+
+    "left menu key": 0xA4,
+    "right menu key": 0xA4,
+
+    "caps lock key": 0x14,
+    "PRINT SCREEN key": 0x2C,
+
 
     "left mouse down": win32con.WM_LBUTTONDOWN,
     "left mouse up": win32con.WM_LBUTTONUP,
@@ -45,6 +80,13 @@ vkey = {
     "left mouse button": win32con.MK_LBUTTON,
     "right mouse button": win32con.MK_RBUTTON,
 }
+
+
+START_FISHING_SLEEP_TIME = 2  # Skip the first x second to reduce false positive rate
+MOVE_MOUSE_SLEEP_TIME = 0.2  # Add some sleep time to avoid moving mouse too fast
+CLICK_MOUSE_SLEEP_TIME = 0.5  # Add some sleep time to avoid clicking mouse too fast
+GENERAL_SLEEP_TIME = 1  # Sleep time for other cases
+BITE_SLEEP_TIME = 5 + 1  # It costs 5s to use the bait
 
 
 if __name__ == '__main__':
